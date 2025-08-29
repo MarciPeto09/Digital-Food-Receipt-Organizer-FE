6906358cc0abe93738a0a25c953e6de8c7694c29
MarciPeto09 <Marcelina.peto09@gmail.com>
2025-08-30 00:01:12 +0100

    Profile nel NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import SearchBar from "./SearchBar";
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import LanguageSelector from './LanguageSelector ';
+import { getUserById } from '../services/userService';
 
 const NavBar = () => {
   const { t } = useTranslation();
   const [userId, setUserId] = useState("");
+  const [photo, setPhoto] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
@@ -21,13 +23,27 @@ const NavBar = () => {
   };
 
   const isAdmin = () => {
-  return localStorage.getItem("userRole") === "ROLE_ADMIN";
-};
+    return localStorage.getItem("userRole") === "ROLE_ADMIN";
+  };
+
+  useEffect(() => {
+  const fetchUser = async () => {
+    const userId = localStorage.getItem("userId");
+    try {
+      const response = await getUserById(userId);                     
+      setPhoto(response.data.photo);              
+    } catch (error) {
+      console.error("Errore nel fetch user:", error);
+    }
+  };
+
+  fetchUser();
+}, []);
+
 
   return (
     <nav className="navbar navbar-light bg-light px-3 py-2 shadow-sm">
       <div className="container-fluid">
-        {/* Top bar */}
         <div className="d-flex justify-content-between align-items-center w-100">
           <button
             className="btn btn-outline-secondary"
@@ -42,6 +58,22 @@ const NavBar = () => {
               <SearchBar />
             </div>
             <LanguageSelector />
+            <Link to={`/profile/${userId}`} className="nav-link ms-2"><img
+              src={`http://localhost:8080/uploads/${photo}`}
+              className="rounded-circle shadow"
+              style={{
+                width: '40px',
+                height: '40px',
+                objectFit: 'cover',
+                border: '4px solid #ffb347',
+                background: '#fffbe6'
+              }}
+              alt="profile"s
+            /></Link>
+
+             <Link to="/basket" className="nav-link ms-2">
+              <i className="bi bi-cart" style={{ fontSize: '1.5rem',color: '#d35400' }}></i>
+            </Link>
           </div>
         </div>
 
@@ -57,14 +89,10 @@ const NavBar = () => {
             <Link to="/analytics" className="nav-link">{t('nav.analytics')}</Link>
             {isAdmin() && (
               <>
-              <Link to="/register" className="nav-link">{t('nav.register')}</Link>
-              <Link to="/registerVendor" className="nav-link">{t('nav.registerVendor')}</Link>
+                <Link to="/register" className="nav-link">{t('nav.register')}</Link>
+                <Link to="/registerVendor" className="nav-link">{t('nav.registerVendor')}</Link>
               </>
-              )}
-            <Link to={`/profile/${userId}`} className="nav-link">{t('nav.profile')}</Link>
-            <Link to="/basket" className="nav-link">
-              <i className="bi bi-cart" style={{ fontSize: '1.5rem' }}></i>
-            </Link>
+            )}
           </div>
         )}
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,21 +3,21 @@ import axios from 'axios';
 const API_URL = 'http://localhost:8080/api/users';
 
 export const registerUser = async (request) => {
-    const respose = await axios.post('API_URL/register');
+    const respose = await axios.post(`API_URL/register`);
     return respose.data;
 }; 
 
 export const getUserById = async (userId) => {
-    const response = await axios.get('API_URL/${userId}');
-    return response.data;   
+  const response = await axios.get(`${API_URL}/${userId}`);
+  return response;
 };
 
 export const getUserReceipts = async (userId) => {
-    const response = await axios.get('API_URL/${userId}/receipts');
+    const response = await axios.get(`API_URL/${userId}/receipts`);
     return response.data;  
 };
 
 export const getUserTotalSpending = async (userId) => {
-   const response = await axios.get('API_URL/${userId}/total-spending');
+   const response = await axios.get(`API_URL/${userId}/total-spending`);
     return response.data; 
 };
