d3b52b434f4727b4fb1a1c3abe245212ccb00f7f
MarciPeto09 <Marcelina.peto09@gmail.com>
2025-09-16 11:45:56 +0100

    fix css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer  = () => {
 const { t } = useTranslation();
 
 return (
-  <nav className="navbar navbar-light shadow-sm border-top">
+  <nav className="footer">
   <div className="container-fluid d-flex justify-content-between">
     <Link to="https://github.com/MarciPeto09" className="nav-link">Marcelina GitHub</Link>
     <Link to="/" className="nav-link">{t('nav.logout')}</Link>
